b634bf1bb8fcf83cdbace0513d39f262e8636eb9
giftifydemo <112926630+giftifydemo@users.noreply.github.com>
2022-09-06 08:50:36 +0200

    Look... Jokes

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
 <?php
-echo "Hello again everyone!";
+  $api_url = "https://v2.jokeapi.dev/joke/Any";
+  $joke = json_decode(file_get_contents($api_url));
+?>
+
+<div class="box-container">
+  <div class="box">
+    <div class="box-content">
+      <div class="tag">
+        <?php echo $joke->category;?>
+      </div>
+      <?php if($joke->type=='single'){?>
+        <span><?php echo $joke->joke;?></span>
+      <?php } else{ ?>
+        <span><?php echo $joke->setup;?></span>
+        <hr>
+        <span><?php echo $joke->delivery;?></span>
+      <?php } ?>
+    </div>
+  </div>
+</div>
